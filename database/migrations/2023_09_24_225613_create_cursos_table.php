d997dc708ac915e7e17dc1b7ca6bee2c8d2a3f95
Yasssmin-M <mpaula.yasmin@gmail.com>
2023-09-29 23:04:05 -0300

    Icones 29.09

--- database/migrations/2023_09_24_225613_create_cursos_table.php
+++ database/migrations/2023_09_24_225613_create_cursos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
-            $table->string('nomecurso');
+            $table->string('nomecurso',40);
             $table->string('cargahoraria');
             $table->unsignedBigInteger('idcategoria');
             $table->decimal('valor',6,2);
--- database/migrations/2023_09_25_230217_create_aulas_table.php
+++ database/migrations/2023_09_25_230217_create_aulas_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('tituloaula');
             $table->string('urlaula');
             $table->timestamps();
-            $table->foreign('idcurso')->references('id')->on('cursos'));
+            $table->foreign('idcurso')->references('id')->on('cursos');
         });
     }
 
