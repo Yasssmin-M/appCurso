af8246dff08b2cf1129b40c71675cdf5fb80b4fe
Yasssmin-M <mpaula.yasmin@gmail.com>
2023-10-02 20:34:08 -0300

    aula 02/10

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/cadcategoria', function () {
+    return view('cad_categoria');
+});
